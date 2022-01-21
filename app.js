c7848d3742e1f1f7918042a22ef0ae671b2f9e29
Rebekah 2021 <rebekahmcdevitt@gmail.com>
2022-01-21 13:50:18 -0500

    fs write file added and page-template

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length);
+const fs = require ('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
 
 const [name, github] = profileDataArgs;
-    Name: ${name}
-    Github: ${githubName}  
 
 // create arrow function
-const generatePage = (name, github) => {
-  return`
-  <!DOCTYPE html>
-  <html lang="en">
-  <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
-console.log(generatePage(name, github));
-console.log(generatePage('Jane', 'janehub'));
+fs.writeFile('index.html', generatePage(name , github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+
+});
 
 
